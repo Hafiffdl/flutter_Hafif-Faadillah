def4df67774b9be1ef80853316d0b9b2eb76a6ca
Hafif Faadillah <hafif.fdl16@gmail.com>
2023-09-07 14:39:55 +0700

    Pengumpulan prioritas2 & screenshot CupertinoApp

---
+++ 10_Platform Widget/pratikum/cupertino_app/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:cupertino_app/cupertino_app.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp (const MyApp());
+}
+
